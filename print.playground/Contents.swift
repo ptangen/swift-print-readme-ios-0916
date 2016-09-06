e440c615e773aa3d001eab650e7e771cd9800b4b
Paul Tangen <ptangen@ptangen.com>
2016-09-06 08:56:06 -0400

    Complete the print lesson.

---
+++ print.playground/Contents.swift
@@ -0,0 +1,17 @@
+
+var favoriteWord = "now"
+var favoriteColor = "blue"
+var favoriteMusician = "Erika"
+
+print(favoriteWord)
+print(favoriteColor)
+print(favoriteMusician)
+
+print("I'm printing a string in Swift!")
+
+print("Hi Mom! My favorite word is serendipity, my favorite color is green, and my favorite musician is Billy Joel.")
+
+let color = "blue"
+print("The sky is \(color).")
+
+print("Hi Mom! My favorite word is \(favoriteWord), my favorite color is \(favoriteColor), and my favorite musician is \(favoriteMusician).")
